4418bd52f1fca52a0d8d5f5b60abd2fdfc7146bd
Rob Mensching <rob@firegiant.com>
2017-08-15 01:31:51 -0700

    Migrate MsgGen

---
+++ src/WixBuildTools.MsgGen/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCulture("")]
