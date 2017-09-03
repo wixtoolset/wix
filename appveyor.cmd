088dc648a3478e2cacdbdab1cb1782556642ee69
Rob Mensching <rob@firegiant.com>
2017-09-03 16:51:39 -0700

    Initial commit

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,5 +1,10 @@
 @setlocal
 @pushd %~dp0
 
+msbuild -p:Configuration=Release;Platform=Win32 -t:PackNativeNuget src\winterop\winterop.vcxproj
+msbuild -p:Configuration=Release;Platform=x64 -t:PackNativeNuget src\winterop\winterop.vcxproj
+
+dotnet pack -c Release .\src\WixToolset.Core.Native\WixToolset.Core.Native.csproj
+
 @popd
 @endlocal
