8e8b7344cee20ca29aee9105beebc63d35551f1b
Rob Mensching <rob@firegiant.com>
2017-09-17 15:22:05 -0700

    Standardize on appveyor.cmd for builds

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,6 +1,8 @@
 @setlocal
 @pushd %~dp0
 
+nuget restore
+
 msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140_xp
 msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140_xp
 
