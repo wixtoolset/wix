feea62d5bd1f90bb012cb7cf2c84c9703f6568b2
Rob Mensching <rob@firegiant.com>
2017-09-24 15:06:23 -0700

    Publish Core tools for internal use

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,7 +1,11 @@
 @setlocal
 @pushd %~dp0
+@set _P=%~dp0build\Release\publish
 
-dotnet pack -c Release
+dotnet publish -c Release -o %_P% -r win-x86 src\wix
+dotnet publish -c Release -o %_P% -r win-x86 src\WixToolset.BuildTasks
+
+dotnet pack -c Release src\WixToolset.Core.InternalPackage
 
 @popd
 @endlocal
