3b6ae0dc3d1af57f5da537d791bbe1dcc77040ca
Bob Arnson <bob@joyofsetup.com>
2018-06-21 20:38:43 -0400

    Update to netcoreapp2.1 and xunit 2.3.1.

--- appveyor.cmd
+++ appveyor.cmd
@@ -6,7 +6,7 @@ dotnet build -c Release src\test\WixToolsetTest.BuildTasks
 dotnet build -c Release src\test\WixToolsetTest.CoreIntegration
 dotnet build -c Release src\test\WixToolsetTest.LightIntegration
 
-dotnet publish -c Release -o %_P%\netcoreapp2.0 -r win-x86 src\wix
+dotnet publish -c Release -o %_P%\netcoreapp2.1 -r win-x86 src\wix
 dotnet publish -c Release -o %_P%\net461 -r win-x86 src\light
 dotnet publish -c Release -o %_P%\net461 -r win-x86 src\WixToolset.BuildTasks
 
