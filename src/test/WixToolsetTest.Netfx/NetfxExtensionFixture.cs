6c33f1b726802fe229a65c9745187c2144459edc
Rob Mensching <rob@firegiant.com>
2018-10-03 22:37:13 -0700

    Adopt new WixRunner execution

--- src/test/WixToolsetTest.Netfx/NetfxExtensionFixture.cs
+++ src/test/WixToolsetTest.Netfx/NetfxExtensionFixture.cs
@@ -25,9 +25,8 @@ namespace WixToolsetTest.Netfx
 
         private static void Build(string[] args)
         {
-            var result = WixRunner.Execute(args, out var messages);
-            Assert.Equal(0, result);
-            Assert.Empty(messages);
+            var result = WixRunner.Execute(args)
+                                  .AssertSuccess();
         }
     }
 }
