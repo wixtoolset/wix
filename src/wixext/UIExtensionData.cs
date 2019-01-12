9d5d059a2147aa0fe38f7103148c020b66455e83
Sean Hall <r.sean.hall@gmail.com>
2019-01-12 11:11:10 -0600

    Import code from old v4 repo

---
+++ src/wixext/UIExtensionData.cs
@@ -0,0 +1,43 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+namespace WixToolset.Extensions
+{
+    using System;
+    using System.Reflection;
+    using WixToolset.Data;
+    using WixToolset.Extensibility;
+
+    /// <summary>
+    /// The WiX Toolset UI Extension.
+    /// </summary>
+    public sealed class UIExtensionData : ExtensionData
+    {
+        /// <summary>
+        /// Gets the default culture.
+        /// </summary>
+        /// <value>The default culture.</value>
+        public override string DefaultCulture
+        {
+            get { return "en-us"; }
+        }
+
+        /// <summary>
+        /// Gets the library associated with this extension.
+        /// </summary>
+        /// <param name="tableDefinitions">The table definitions to use while loading the library.</param>
+        /// <returns>The loaded library.</returns>
+        public override Library GetLibrary(TableDefinitionCollection tableDefinitions)
+        {
+            return UIExtensionData.GetExtensionLibrary(tableDefinitions);
+        }
+
+        /// <summary>
+        /// Internal mechanism to access the extension's library.
+        /// </summary>
+        /// <returns>Extension's library.</returns>
+        internal static Library GetExtensionLibrary(TableDefinitionCollection tableDefinitions)
+        {
+            return ExtensionData.LoadLibraryHelper(Assembly.GetExecutingAssembly(), "WixToolset.Extensions.Data.ui.wixlib", tableDefinitions);
+        }
+    }
+}
