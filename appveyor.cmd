f8c806374a2fdbf8fb85a654d561e37a419f5ea3
Sean Hall <r.sean.hall@gmail.com>
2019-01-13 18:51:12 -0600

    Add ARM and ARM64 for v141.

--- appveyor.cmd
+++ appveyor.cmd
@@ -8,6 +8,8 @@ msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140_xp
 
 msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp
 msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141_xp
+msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141
+msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141
 
 msbuild -p:Configuration=Release -t:PackNativeNuget src\dutil\dutil.vcxproj
 
