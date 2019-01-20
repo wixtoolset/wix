4d30ab70573f9734d7fd3cd4d54c02173fa281db
Sean Hall <r.sean.hall@gmail.com>
2019-01-20 09:19:12 -0600

    Import code from old v4 repo

---
+++ src/ca/cost.h
@@ -0,0 +1,5 @@
+#pragma once
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+
+const UINT COST_HTTP_URL_ACL = 2000;
---
+++ src/wixext/HttpConstants.cs
@@ -0,0 +1,19 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+namespace WixToolset.Extensions
+{
+    using System;
+
+    internal static class HttpConstants
+    {
+        // from winnt.h
+        public const int GENERIC_ALL = 0x10000000;
+        public const int GENERIC_EXECUTE = 0x20000000;
+        public const int GENERIC_WRITE = 0x40000000;
+
+        // from wixhttpca.cpp
+        public const int heReplace = 0;
+        public const int heIgnore = 1;
+        public const int heFail = 2;
+    }
+}
