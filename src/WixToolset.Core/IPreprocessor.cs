bcd212a2ef41459789528c5956d8e6abd1b7d405
Bob Arnson <bob@firegiant.com>
2019-02-26 21:21:04 -0500

    Make `IPreprocessor` public as it should be.

--- src/WixToolset.Core/IPreprocessor.cs
+++ src/WixToolset.Core/IPreprocessor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
 namespace WixToolset.Core
 {
@@ -6,7 +6,7 @@ namespace WixToolset.Core
     using System.Xml.Linq;
     using WixToolset.Extensibility.Data;
 
-    internal interface IPreprocessor
+    public interface IPreprocessor
     {
         XDocument Preprocess(IPreprocessContext context);
 
