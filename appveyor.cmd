1ec68f7c9925536e9f11f5f849eb812a99dfa8ce
Rob Mensching <rob@firegiant.com>
2019-05-09 15:12:54 -0700

    Support VS2019

--- appveyor.cmd
+++ appveyor.cmd
@@ -8,8 +8,13 @@ msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140_xp
 
 msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp
 msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141_xp
-msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141
-msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141
+rem msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141
+rem msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141
+
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v142
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v142
+rem msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v142
+rem msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v142
 
 msbuild -p:Configuration=Release -t:PackNativeNuget src\dutil\dutil.vcxproj
 
