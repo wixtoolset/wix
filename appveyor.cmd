20bbfc8ad82e6246f5357d8bc183a5fc0ea3fac7
Sean Hall <r.sean.hall@gmail.com>
2019-12-11 20:15:08 +1100

    Setup WixToolset.Mba.Core project and nuget package.

--- appveyor.cmd
+++ appveyor.cmd
@@ -8,6 +8,7 @@ msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140_xp
 msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp
 
 msbuild -p:Configuration=Release -t:Pack src\balutil\balutil.vcxproj
+msbuild -p:Configuration=Release -t:Pack src\WixToolset.Mba.Core\WixToolset.Mba.Core.csproj
 
 @popd
 @endlocal
