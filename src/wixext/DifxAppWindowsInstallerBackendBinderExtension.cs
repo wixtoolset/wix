7132f1882ebcfeb0113730bd722fa3587bc5e5f4
Sean Hall <r.sean.hall@gmail.com>
2020-04-08 14:06:02 +1000

    Update dependencies.

--- src/wixext/DifxAppWindowsInstallerBackendBinderExtension.cs
+++ src/wixext/DifxAppWindowsInstallerBackendBinderExtension.cs
@@ -2,6 +2,7 @@
 
 namespace WixToolset.DifxApp
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Xml;
     using WixToolset.Data.WindowsInstaller;
@@ -11,7 +12,7 @@ namespace WixToolset.DifxApp
     {
         private static readonly TableDefinition[] Tables = LoadTables();
 
-        protected override TableDefinition[] TableDefinitionsForTuples => Tables;
+        public override IEnumerable<TableDefinition> TableDefinitions => Tables;
 
         private static TableDefinition[] LoadTables()
         {
