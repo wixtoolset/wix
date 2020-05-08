23f8f2d3b18f36478e0af9e72ec6fba3a200052d
Sean Hall <r.sean.hall@gmail.com>
2020-05-08 22:06:35 +1000

    Remove heat messages

--- src/wixext/UtilErrors.cs
+++ src/wixext/UtilErrors.cs
@@ -8,31 +8,6 @@ namespace WixToolset.Util
 
     public static class UtilErrors
     {
-        public static Message ArgumentRequiresValue(string argument)
-        {
-            return Message(null, Ids.ArgumentRequiresValue, "The argument '{0}' does not have a value specified and it is required.", argument);
-        }
-
-        public static Message DirectoryNotFound(string directory)
-        {
-            return Message(null, Ids.DirectoryNotFound, "The directory '{0}' could not be found.", directory);
-        }
-
-        public static Message EmptyDirectory(string directory)
-        {
-            return Message(null, Ids.EmptyDirectory, "The directory '{0}' did not contain any files or sub-directories and since empty directories are not being kept, there was nothing to harvest.", directory);
-        }
-
-        public static Message ErrorTransformingHarvestedWiX(string transform, string message)
-        {
-            return Message(null, Ids.ErrorTransformingHarvestedWiX, "Error applying transform {0} to harvested WiX: {1}", transform, message);
-        }
-
-        public static Message FileNotFound(string file)
-        {
-            return Message(null, Ids.FileNotFound, "The file '{0}' cannot be found.", file);
-        }
-
         public static Message IllegalAttributeWithoutComponent(SourceLineNumber sourceLineNumbers, string elementName, string attributeName)
         {
             return Message(sourceLineNumbers, Ids.IllegalAttributeWithoutComponent, "The {0}/@{1} attribute cannot be specified unless the element has a Component as an ancestor. A {0} that does not have a Component ancestor is not installed.", elementName, attributeName);
@@ -53,26 +28,6 @@ namespace WixToolset.Util
             return Message(sourceLineNumbers, Ids.InvalidRegistryObject, "The {0} element has no id and cannot have its permissions set. If you want to set permissions on a 'placeholder' registry key, force its creation by setting the ForceCreateOnInstall attribute to yes.", registryElementName);
         }
 
-        public static Message PerformanceCategoryNotFound(string key)
-        {
-            return Message(null, Ids.PerformanceCategoryNotFound, "Performance category '{0}' not found.", key);
-        }
-
-        public static Message SpacesNotAllowedInArgumentValue(string arg, string value)
-        {
-            return Message(null, Ids.SpacesNotAllowedInArgumentValue, "The switch '{0}' does not allow the spaces from the value. Please remove the spaces in from the value: {1}", arg, value);
-        }
-
-        public static Message UnableToOpenRegistryKey(string key)
-        {
-            return Message(null, Ids.UnableToOpenRegistryKey, "Unable to open registry key '{0}'.", key);
-        }
-
-        public static Message UnsupportedPerformanceCounterType(string key)
-        {
-            return Message(null, Ids.UnsupportedPerformanceCounterType, "Unsupported performance counter type '{0}'.", key);
-        }
-
         private static Message Message(SourceLineNumber sourceLineNumber, Ids id, string format, params object[] args)
         {
             return new Message(sourceLineNumber, MessageLevel.Error, (int)id, format, args);
@@ -87,16 +42,7 @@ namespace WixToolset.Util
         {
             IllegalAttributeWithoutComponent = 5050,
             IllegalElementWithoutComponent = 5051,
-            DirectoryNotFound = 5052,
-            EmptyDirectory = 5053,
             IllegalFileValueInPerfmonOrManifest = 5054,
-            ErrorTransformingHarvestedWiX = 5055,
-            UnableToOpenRegistryKey = 5056,
-            SpacesNotAllowedInArgumentValue = 5057,
-            ArgumentRequiresValue = 5058,
-            FileNotFound = 5059,
-            PerformanceCategoryNotFound = 5060,
-            UnsupportedPerformanceCounterType = 5061,
             InvalidRegistryObject = 5063,
         }
     }
--- src/wixext/UtilWarnings.cs
+++ src/wixext/UtilWarnings.cs
@@ -8,44 +8,14 @@ namespace WixToolset.Util
 
     public static class UtilWarnings
     {
-        public static Message AssemblyHarvestFailed(string file, string message)
-        {
-            return Message(null, Ids.AssemblyHarvestFailed, "Could not harvest data from a file that was expected to be an assembly: {0}. If this file is not an assembly you can ignore this warning. Otherwise, this error detail may be helpful to diagnose the failure: {1}", file, message);
-        }
-
         public static Message DeprecatedPerfCounterElement(SourceLineNumber sourceLineNumbers)
         {
             return Message(sourceLineNumbers, Ids.DeprecatedPerfCounterElement, "The PerfCounter element has been deprecated.  Please use the PerformanceCounter element instead.");
         }
 
-        public static Message DuplicateDllRegistryEntry(string registryKey, string componentId)
-        {
-            return Message(null, Ids.DuplicateDllRegistryEntry, "Ignoring the registry key '{0}', it has already been added to the component '{1}'.", registryKey, componentId);
-        }
-
-        public static Message DuplicateDllRegistryEntry(string registryKey, string registryKeyValue, string componentId)
-        {
-            return Message(null, Ids.DuplicateDllRegistryEntry, "Ignoring the registry key '{0}', it has already been added to the component '{2}'. The registry key value '{1}' will not be harvested.", registryKey, registryKeyValue, componentId);
-        }
-
         public static Message RequiredAttributeForWindowsXP(SourceLineNumber sourceLineNumbers, string elementName, string attributeName)
         {
             return Message(sourceLineNumbers, Ids.RequiredAttributeForWindowsXP, "The {0}/@{1} attribute must be specified to successfully install on Windows XP.  You can ignore this warning if this installation does not install on Windows XP.", elementName, attributeName);
-        }
-
-        public static Message SelfRegHarvestFailed(string file, string message)
-        {
-            return Message(null, Ids.SelfRegHarvestFailed, "Could not harvest data from a file that was expected to be a SelfReg DLL: {0}. If this file does not support SelfReg you can ignore this warning. Otherwise, this error detail may be helpful to diagnose the failure: {1}", file, message);
-        }
-
-        public static Message TypeLibLoadFailed(string file, string message)
-        {
-            return Message(null, Ids.TypeLibLoadFailed, "Could not load file that was expected to be a type library based off of file extension: {0}. If this file is not a type library you can ignore this warning. Otherwise, this error detail may be helpful to diagnose the load failure: {1}", file, message);
-        }
-
-        public static Message UnsupportedRegistryType(string registryValue, int regFileLineNumber, string unsupportedType)
-        {
-            return Message(null, Ids.UnsupportedRegistryType, "Ignoring the registry value '{0}' found on line {1}, because it is of a type unsupported by Windows Installer ({2}).", registryValue, regFileLineNumber, unsupportedType);
         }
 
         private static Message Message(SourceLineNumber sourceLineNumber, Ids id, string format, params object[] args)
@@ -60,13 +30,8 @@ namespace WixToolset.Util
 
         public enum Ids
         {
-            SelfRegHarvestFailed = 5150,
-            AssemblyHarvestFailed = 5151,
-            TypeLibLoadFailed = 5152,
             DeprecatedPerfCounterElement = 5153,
             RequiredAttributeForWindowsXP = 5154,
-            DuplicateDllRegistryEntry = 5156,
-            UnsupportedRegistryType = 5157,
         }
     }
 }
