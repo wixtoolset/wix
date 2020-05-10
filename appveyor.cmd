bfc587c1b4d1f96d8b7c595915152d5b6a5c01a6
Bob Arnson <bob@joyofsetup.com>
2020-05-10 17:21:54 -0400

    Drop XP support and enable ARM64.

--- appveyor.cmd
+++ appveyor.cmd
@@ -3,13 +3,18 @@
 
 nuget restore
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140_xp
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140_xp
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141_xp
-msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141
-msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141
+REM msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141
+REM msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141
+
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v142
+msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v142
+msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v142
+msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v142
 
 msbuild -p:Configuration=Release -t:PackNativeNuget src\wcautil\wcautil.vcxproj
 
