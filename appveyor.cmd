b0b50f1d8512b6d7ebd087d19406629db0a5ef9b
Sean Hall <r.sean.hall@gmail.com>
2020-05-15 13:48:12 +1000

    Drop XP support.

--- appveyor.cmd
+++ appveyor.cmd
@@ -3,9 +3,9 @@
 
 nuget restore
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140_xp
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141_xp
+msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141
 
 msbuild -p:Configuration=Release -t:Pack src\balutil\balutil.vcxproj
 msbuild -p:Configuration=Release -t:Pack src\bextutil\bextutil.vcxproj
