0b9fd5ae84460d943e685b9821473f356a66bd3b
paulomorgado <470455+paulomorgado@users.noreply.github.com>
2020-05-25 13:13:25 +0100

    Allow more than one harvested file and files differing by extension add component and directory for more disambiguation Fix HarvestFile Ouput added unit tests

--- src/test/WixToolsetTest.BuildTasks/MsbuildHeatFixture.cs
+++ src/test/WixToolsetTest.BuildTasks/MsbuildHeatFixture.cs
@@ -41,7 +41,7 @@ namespace WixToolsetTest.BuildTasks
                 var warnings = result.Output.Where(line => line.Contains(": warning"));
                 Assert.Empty(warnings);
 
-                var generatedFilePath = Path.Combine(intermediateFolder, @"_HeatFilePackage_file.wxs");
+                var generatedFilePath = Path.Combine(intermediateFolder, @"_ProductComponents_INSTALLFOLDER_HeatFilePackage.wixproj_file.wxs");
                 Assert.True(File.Exists(generatedFilePath));
 
                 var generatedContents = File.ReadAllText(generatedFilePath);
@@ -69,6 +69,83 @@ namespace WixToolsetTest.BuildTasks
 
                 var fileTuple = section.Tuples.OfType<FileTuple>().Single();
                 Assert.Equal(@"SourceDir\HeatFilePackage.wixproj", fileTuple[FileTupleFields.Source].PreviousValue.AsPath().Path);
+            }
+        }
+
+        [Fact]
+        public void CanBuildHeatFileWithMultipleFilesPackage()
+        {
+            var projectPath = TestData.Get(@"TestData\HeatFileMultpleFilesSameFileName\HeatFileMultpleFilesSameFileName.wixproj");
+
+            using (var fs = new DisposableFileSystem())
+            {
+                var baseFolder = fs.GetFolder();
+                var binFolder = Path.Combine(baseFolder, @"bin\");
+                var intermediateFolder = Path.Combine(baseFolder, @"obj\");
+
+                var result = MsbuildRunner.Execute(projectPath, new[]
+                {
+                    $"-p:WixTargetsPath={WixTargetsPath}",
+                    $"-p:IntermediateOutputPath={intermediateFolder}",
+                    $"-p:OutputPath={binFolder}"
+                });
+                result.AssertSuccess();
+
+                var heatCommandLines = result.Output.Where(line => line.TrimStart().StartsWith("heat.exe file"));
+                Assert.Equal(2, heatCommandLines.Count());
+
+                var warnings = result.Output.Where(line => line.Contains(": warning"));
+                Assert.Empty(warnings);
+
+                var generatedFilePath = Path.Combine(intermediateFolder, @"_TxtProductComponents_INSTALLFOLDER_MyProgram.txt_file.wxs");
+                Assert.True(File.Exists(generatedFilePath));
+
+                var generatedContents = File.ReadAllText(generatedFilePath);
+                var testXml = generatedContents.GetTestXml();
+                Assert.Equal("<Wix>" +
+                    "<Fragment>" +
+                    "<DirectoryRef Id='INSTALLFOLDER'>" +
+                    "<Component Id='MyProgram.txt' Guid='*'>" +
+                    @"<File Id='MyProgram.txt' KeyPath='yes' Source='SourceDir\MyProgram.txt' />" +
+                    "</Component>" +
+                    "</DirectoryRef>" +
+                    "</Fragment>" +
+                    "<Fragment>" +
+                    "<ComponentGroup Id='TxtProductComponents'>" +
+                    "<ComponentRef Id='MyProgram.txt' />" +
+                    "</ComponentGroup>" +
+                    "</Fragment>" +
+                    "</Wix>", testXml);
+
+                generatedFilePath = Path.Combine(intermediateFolder, @"_JsonProductComponents_INSTALLFOLDER_MyProgram.json_file.wxs");
+                Assert.True(File.Exists(generatedFilePath));
+
+                generatedContents = File.ReadAllText(generatedFilePath);
+                testXml = generatedContents.GetTestXml();
+                Assert.Equal("<Wix>" +
+                    "<Fragment>" +
+                    "<DirectoryRef Id='INSTALLFOLDER'>" +
+                    "<Component Id='MyProgram.json' Guid='*'>" +
+                    @"<File Id='MyProgram.json' KeyPath='yes' Source='SourceDir\MyProgram.json' />" +
+                    "</Component>" +
+                    "</DirectoryRef>" +
+                    "</Fragment>" +
+                    "<Fragment>" +
+                    "<ComponentGroup Id='JsonProductComponents'>" +
+                    "<ComponentRef Id='MyProgram.json' />" +
+                    "</ComponentGroup>" +
+                    "</Fragment>" +
+                    "</Wix>", testXml);
+
+                var pdbPath = Path.Combine(binFolder, "HeatFileMultpleFilesSameFileName.wixpdb");
+                Assert.True(File.Exists(pdbPath));
+
+                var intermediate = Intermediate.Load(pdbPath);
+                var section = intermediate.Sections.Single();
+
+                var fileTuples = section.Tuples.OfType<FileTuple>().ToArray();
+                Assert.Equal(@"SourceDir\MyProgram.txt", fileTuples[0][FileTupleFields.Source].PreviousValue.AsPath().Path);
+                Assert.Equal(@"SourceDir\MyProgram.json", fileTuples[1][FileTupleFields.Source].PreviousValue.AsPath().Path);
             }
         }
     }
