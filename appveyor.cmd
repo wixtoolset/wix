90f501f6ad35a060fbb404f6a3eeaf1ac50e1a3c
Rob Mensching <rob@firegiant.com>
2020-06-04 10:25:04 -0700

    Fail build if any command fails in the build batch file

--- appveyor.cmd
+++ appveyor.cmd
@@ -2,13 +2,13 @@
 @pushd %~dp0
 @set _P=%~dp0build\Release\publish
 
-dotnet test -c Release src\test\WixToolsetTest.CoreIntegration
+dotnet test -c Release src\test\WixToolsetTest.CoreIntegration || exit /b %ERRORLEVEL%
 
-dotnet pack -c Release src\WixToolset.Core
-dotnet pack -c Release src\WixToolset.Core.Burn
-dotnet pack -c Release src\WixToolset.Core.WindowsInstaller
+dotnet pack -c Release src\WixToolset.Core || exit /b %ERRORLEVEL%
+dotnet pack -c Release src\WixToolset.Core.Burn || exit /b %ERRORLEVEL%
+dotnet pack -c Release src\WixToolset.Core.WindowsInstaller || exit /b %ERRORLEVEL%
 
-dotnet pack -c Release src\WixToolset.Core.TestPackage
+dotnet pack -c Release src\WixToolset.Core.TestPackage || exit /b %ERRORLEVEL%
 
 @popd
 @endlocal
