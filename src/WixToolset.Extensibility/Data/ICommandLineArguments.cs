ce397dd68b6699092d0b038b9a8c353f1f6a425d
Rob Mensching <rob@firegiant.com>
2020-06-13 09:53:28 -0700

    Introduce simplified command creation methods plus xmldoc

--- src/WixToolset.Extensibility/Data/ICommandLineArguments.cs
+++ src/WixToolset.Extensibility/Data/ICommandLineArguments.cs
@@ -1,9 +1,12 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
 namespace WixToolset.Extensibility.Data
 {
     using WixToolset.Extensibility.Services;
 
+    /// <summary>
+    /// Parsed command-line arguments.
+    /// </summary>
     public interface ICommandLineArguments
     {
         string[] OriginalArguments { get; set; }
@@ -14,10 +17,22 @@ namespace WixToolset.Extensibility.Data
 
         string ErrorArgument { get; set; }
 
+        /// <summary>
+        /// Populate this argument from a string.
+        /// </summary>
+        /// <param name="commandLine">String to parse.</param>
         void Populate(string commandLine);
 
+        /// <summary>
+        /// Populate this argument from array of strings.
+        /// </summary>
+        /// <param name="args">Array of strings.</param>
         void Populate(string[] args);
 
+        /// <summary>
+        /// Parses this arguments after it is populated.
+        /// </summary>
+        /// <returns>Parser for this arguments.</returns>
         ICommandLineParser Parse();
     }
 }
--- src/WixToolset.Extensibility/Services/ICommandLine.cs
+++ src/WixToolset.Extensibility/Services/ICommandLine.cs
@@ -1,15 +1,33 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
 namespace WixToolset.Extensibility.Services
 {
     using WixToolset.Extensibility.Data;
 
+    /// <summary>
+    /// Command-line parsing mechanism.
+    /// </summary>
     public interface ICommandLine
     {
-        IExtensionManager ExtensionManager { get; set; }
+        /// <summary>
+        /// Simple way to parse arguments and create a command.
+        /// </summary>
+        /// <param name="args">Unparsed arguments.</param>
+        /// <returns>Command if the command-line arguments can be parsed, otherwise null.</returns>
+        ICommandLineCommand CreateCommand(string[] args);
 
-        ICommandLineArguments Arguments { get; set; }
+        /// <summary>
+        /// Simple way to parse arguments and create a command.
+        /// </summary>
+        /// <param name="commandLine">Unparsed arguments.</param>
+        /// <returns>Command if the command-line arguments can be parsed, otherwise null.</returns>
+        ICommandLineCommand CreateCommand(string commandLine);
 
-        ICommandLineCommand ParseStandardCommandLine();
+        /// <summary>
+        /// Creates a command from populated arguments.
+        /// </summary>
+        /// <param name="arguments">Parsed arguments.</param>
+        /// <returns>Command if the command-line arguments can be parsed, otherwise null.</returns>
+        ICommandLineCommand ParseStandardCommandLine(ICommandLineArguments arguments);
     }
 }
