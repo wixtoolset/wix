949b66aef724f7eef8fe9f241259a146bc649660
Sean Hall <r.sean.hall@gmail.com>
2020-06-15 21:54:23 +1000

    Update appveyor.cmd to not specify error code on early exit.

--- appveyor.cmd
+++ appveyor.cmd
@@ -2,14 +2,14 @@
 @pushd %~dp0
 @set _P=%~dp0build\Release\publish
 
-dotnet test -c Release src\test\WixToolsetTest.CoreIntegration || exit /b %ERRORLEVEL%
+dotnet test -c Release src\test\WixToolsetTest.CoreIntegration || exit /b
 
-dotnet pack -c Release src\WixToolset.Core || exit /b %ERRORLEVEL%
-dotnet pack -c Release src\WixToolset.Core.Burn || exit /b %ERRORLEVEL%
-dotnet pack -c Release src\WixToolset.Core.ExtensionCache || exit /b %ERRORLEVEL%
-dotnet pack -c Release src\WixToolset.Core.WindowsInstaller || exit /b %ERRORLEVEL%
+dotnet pack -c Release src\WixToolset.Core || exit /b
+dotnet pack -c Release src\WixToolset.Core.Burn || exit /b
+dotnet pack -c Release src\WixToolset.Core.ExtensionCache || exit /b
+dotnet pack -c Release src\WixToolset.Core.WindowsInstaller || exit /b
 
-dotnet pack -c Release src\WixToolset.Core.TestPackage || exit /b %ERRORLEVEL%
+dotnet pack -c Release src\WixToolset.Core.TestPackage || exit /b
 
 @popd
 @endlocal
