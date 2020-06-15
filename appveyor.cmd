19be63b9a7c50e17386cd476c1ffd7c40ba9eaf5
Sean Hall <r.sean.hall@gmail.com>
2020-06-15 21:49:43 +1000

    Fail build for each command.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,8 +1,8 @@
 @setlocal
 @pushd %~dp0
 
-dotnet test -c Release src\test\WixToolsetTest.Data\WixToolsetTest.Data.csproj
-dotnet pack -c Release
+dotnet test -c Release src\test\WixToolsetTest.Data\WixToolsetTest.Data.csproj || exit /b
+dotnet pack -c Release || exit /b
 
 @popd
 @endlocal
