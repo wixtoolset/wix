1571b2e9a0dc0925e8d4bfacc19d7acd29068ff8
Sean Hall <r.sean.hall@gmail.com>
2020-06-18 21:58:51 +1000

    Add v142, x64, ARM, and ARM64.

--- appveyor.cmd
+++ appveyor.cmd
@@ -5,8 +5,17 @@
 nuget restore
 
 msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v140
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v140
 
 msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v141
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v141
+msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v141
+msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v141
+
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v142
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v142
+msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v142
+msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v142
 
 @rem msbuild -t:VSTest -p:Configuration=%_C% src\test\WixToolsetTest.Mba.Core\WixToolsetTest.Mba.Core.csproj
 
