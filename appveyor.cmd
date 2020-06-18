04c8a362c9b001811ef5406716667eb73163aa10
Sean Hall <r.sean.hall@gmail.com>
2020-06-18 22:02:10 +1000

    Fail build on each command.

--- appveyor.cmd
+++ appveyor.cmd
@@ -2,27 +2,27 @@
 @pushd %~dp0
 @set _C=Release
 
-nuget restore
+nuget restore || exit /b
 
-msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v140
-msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v140
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v140 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v140 || exit /b
 
-msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v141
-msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v141
-msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v141
-msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v141
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v141 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v141 || exit /b
+msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v141 || exit /b
+msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v141 || exit /b
 
-msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v142
-msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v142
-msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v142
-msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v142
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v142 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v142 || exit /b
+msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v142 || exit /b
+msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v142 || exit /b
 
-@rem msbuild -t:VSTest -p:Configuration=%_C% src\test\WixToolsetTest.Mba.Core\WixToolsetTest.Mba.Core.csproj
+@rem msbuild -t:VSTest -p:Configuration=%_C% src\test\WixToolsetTest.Mba.Core\WixToolsetTest.Mba.Core.csproj || exit /b
 
-msbuild -t:Pack -p:Configuration=%_C% src\balutil\balutil.vcxproj
-msbuild -t:Pack -p:Configuration=%_C% src\bextutil\bextutil.vcxproj
-msbuild -t:Pack -p:Configuration=%_C% src\WixToolset.Mba.Core\WixToolset.Mba.Core.csproj
-msbuild -t:Pack -p:Configuration=%_C% src\mbanative\mbanative.vcxproj
+msbuild -t:Pack -p:Configuration=%_C% src\balutil\balutil.vcxproj || exit /b
+msbuild -t:Pack -p:Configuration=%_C% src\bextutil\bextutil.vcxproj || exit /b
+msbuild -t:Pack -p:Configuration=%_C% src\WixToolset.Mba.Core\WixToolset.Mba.Core.csproj || exit /b
+msbuild -t:Pack -p:Configuration=%_C% src\mbanative\mbanative.vcxproj || exit /b
 
 @popd
 @endlocal
