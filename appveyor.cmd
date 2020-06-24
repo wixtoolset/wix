c50a0e000ab1812ec4781ace349a5d84fa01c8e4
Sean Hall <r.sean.hall@gmail.com>
2020-06-24 14:23:51 +1000

    Merge the native nupkg into the Core.Native nupkg.
    There was no benefit in separating the native assets into its own package, the runtime.<rid>.PackageName strategy is for binaries to be copied to the root of project and therefore doesn't support the portable scenario where all available assets are required.
    
    Manually author the repository element in the nupkg for SourceLink.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,13 +1,13 @@
 @setlocal
 @pushd %~dp0
+@set _C=Release
 
 nuget restore || exit /b
 
-msbuild -p:Configuration=Release .\src\test\WixToolsetTest.Core.Native\WixToolsetTest.Core.Native.csproj || exit /b
+msbuild -p:Configuration=%_C% src\test\WixToolsetTest.Core.Native\WixToolsetTest.Core.Native.csproj || exit /b
+dotnet test -c %_C% --no-build src\test\WixToolsetTest.Core.Native\WixToolsetTest.Core.Native.csproj || exit /b
 
-msbuild -t:Pack -p:Configuration=Release .\src\WixToolset.Core.Native\WixToolset.Core.Native.csproj || exit /b
-
-msbuild -t:Pack -p:Configuration=Release .\src\wixnative\wixnative.vcxproj || exit /b
+msbuild -t:Pack -p:Configuration=%_C% src\WixToolset.Core.Native\WixToolset.Core.Native.csproj || exit /b
 
 @popd
 @endlocal
