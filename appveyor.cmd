dea3a630b6ae10663dd6f7d855541f7f631a2b55
Sean Hall <r.sean.hall@gmail.com>
2020-06-24 18:47:38 +1000

    Update dependencies.
    Run tests before packing and don't build twice.

--- appveyor.cmd
+++ appveyor.cmd
@@ -7,12 +7,10 @@ nuget restore || exit /b
 msbuild -p:Configuration=%_C% -t:Restore || exit /b
 
 msbuild -p:Configuration=%_C% || exit /b
+dotnet test -c %_C% --no-build src\test\WixToolsetTest.Bal || exit /b
 
 msbuild -p:Configuration=%_C% -t:Pack src\wixext\WixToolset.Bal.wixext.csproj || exit /b
 msbuild -p:Configuration=%_C% -t:Pack src\WixToolset.Mba.Host\WixToolset.Mba.Host.csproj || exit /b
-
-msbuild -p:Configuration=%_C% src\test\WixToolsetTest.Bal\WixToolsetTest.Bal.csproj || exit /b
-dotnet test -c %_C% --no-build src\test\WixToolsetTest.Bal || exit /b
 
 @popd
 @endlocal
