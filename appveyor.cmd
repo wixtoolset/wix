c95213afde7af57af4b2bbaa2d29344accbebf01
Rob Mensching <rob@firegiant.com>
2020-06-26 11:57:56 -0700

    Build wixlib with WixToolset.Sdk

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,14 +1,14 @@
 @setlocal
 @pushd %~dp0
 
-nuget restore
+nuget restore || exit /b
 
-msbuild -p:Configuration=Release -t:Restore
+msbuild -p:Configuration=Release -t:Restore || exit /b
 
-msbuild -p:Configuration=Release src\test\WixToolsetTest.Util\WixToolsetTest.Util.csproj
-dotnet test -c Release --no-build src\test\WixToolsetTest.Util
+msbuild -p:Configuration=Release src\test\WixToolsetTest.Util\WixToolsetTest.Util.csproj || exit /b
+dotnet test -c Release --no-build src\test\WixToolsetTest.Util || exit /b
 
-msbuild -p:Configuration=Release -t:Pack src\wixext\WixToolset.Util.wixext.csproj
+msbuild -p:Configuration=Release -t:Pack src\wixext\WixToolset.Util.wixext.csproj || exit /b
 
 @popd
 @endlocal
