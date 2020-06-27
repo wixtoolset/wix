f40f13e06e72b88e7d9602aa64494238a697d1a7
Rob Mensching <rob@firegiant.com>
2020-06-27 14:55:22 -0700

    Improve build batch file

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,13 +1,14 @@
 @setlocal
 @pushd %~dp0
 
-nuget restore
+nuget restore || exit /b
 
-msbuild -p:Configuration=Release -t:Restore
+msbuild -p:Configuration=Release -t:Restore || exit /b
 
-msbuild -p:Configuration=Release src\test\WixToolsetTest.Msmq\WixToolsetTest.Msmq.csproj
+msbuild -p:Configuration=Release src\test\WixToolsetTest.Msmq\WixToolsetTest.Msmq.csproj || exit /b
+dotnet test -c Release --no-build src\test\WixToolsetTest.Msmq || exit /b
 
-msbuild -p:Configuration=Release -t:Pack src\wixext\WixToolset.Msmq.wixext.csproj
+msbuild -p:Configuration=Release -t:Pack src\wixext\WixToolset.Msmq.wixext.csproj || exit /b
 
 @popd
 @endlocal
