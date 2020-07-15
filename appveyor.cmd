0671c1c92c26bd23a422de6b8c6d8beeabb0f2db
Sean Hall <r.sean.hall@gmail.com>
2020-07-15 17:33:43 +1000

    Add WixBuildTools.TestSupport.Native.nupkg.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,7 +1,13 @@
 @setlocal
 @pushd %~dp0
 
-dotnet pack -c Release
+nuget restore || exit /b
+
+dotnet pack -c Release src\WixBuildTools.MsgGen\WixBuildTools.MsgGen.csproj || exit /b
+dotnet pack -c Release src\WixBuildTools.TestSupport\WixBuildTools.TestSupport.csproj || exit /b
+dotnet pack -c Release src\WixBuildTools.XsdGen\WixBuildTools.XsdGen.csproj || exit /b
+
+msbuild -p:Configuration=Release -t:PackNativeNuget src\WixBuildTools.TestSupport.Native\WixBuildTools.TestSupport.Native.vcxproj || exit /b
 
 @popd
 @endlocal
