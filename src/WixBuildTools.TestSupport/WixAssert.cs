5f3e7559016997afed60d5003fd11548c3b24e3a
Sean Hall <r.sean.hall@gmail.com>
2020-07-16 16:39:27 +1000

    Add WixAssert.AssertEqualLineByLine.

--- src/WixBuildTools.TestSupport/WixAssert.cs
+++ src/WixBuildTools.TestSupport/WixAssert.cs
@@ -7,6 +7,16 @@ namespace WixBuildTools.TestSupport
 
     public class WixAssert : Assert
     {
+        public static void AssertEqualLineByLine(string[] expectedLines, string[] actualLines)
+        {
+            for (var i = 0; i < expectedLines.Length; ++i)
+            {
+                Assert.True(actualLines.Length > i, $"{i}: expectedLines longer than actualLines");
+                Assert.Equal($"{i}: {expectedLines[i]}", $"{i}: {actualLines[i]}");
+            }
+            Assert.True(expectedLines.Length == actualLines.Length, "actualLines longer than expectedLines");
+        }
+
         public static void Succeeded(int hr, string format, params object[] formatArgs)
         {
             if (0 > hr)
