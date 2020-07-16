0db15d84eaabf2f1950784655ddd79413cfea44f
Sean Hall <r.sean.hall@gmail.com>
2020-07-16 21:17:15 +1000

    Create failing test for named bind paths.

--- src/test/WixToolsetTest.CoreIntegration/WixlibFixture.cs
+++ src/test/WixToolsetTest.CoreIntegration/WixlibFixture.cs
@@ -88,6 +88,36 @@ namespace WixToolsetTest.CoreIntegration
             }
         }
 
+        [Fact(Skip = "Test demonstrates failure")]
+        public void CantBuildWixlibWithBinariesFromMissingNamedBindPaths()
+        {
+            var folder = TestData.Get(@"TestData\WixlibWithBinaries");
+
+            using (var fs = new DisposableFileSystem())
+            {
+                var baseFolder = fs.GetFolder();
+                var intermediateFolder = Path.Combine(baseFolder, "obj");
+                var wixlibPath = Path.Combine(intermediateFolder, @"test.wixlib");
+
+                var result = WixRunner.Execute(new[]
+                {
+                    "build",
+                    Path.Combine(folder, "PackageComponents.wxs"),
+                    "-bf",
+                    "-bindpath", Path.Combine(folder, "data"),
+                    // Use names that aren't excluded in default .gitignores.
+                    "-bindpath", $"AlphaBits={Path.Combine(folder, "data", "alpha")}",
+                    "-bindpath", $"PowerBits={Path.Combine(folder, "data", "powerpc")}",
+                    "-bindpath", $"{Path.Combine(folder, "data", "alpha")}",
+                    "-bindpath", $"{Path.Combine(folder, "data", "powerpc")}",
+                    "-intermediateFolder", intermediateFolder,
+                    "-o", wixlibPath,
+                });
+
+                Assert.InRange(result.ExitCode, 2, int.MaxValue);
+            }
+        }
+
         [Fact]
         public void CanBuildSingleFileUsingWixlib()
         {
