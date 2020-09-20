665654b4f553c567b11aab3b4d65635b083a5a9e
Bob Arnson <bob@firegiant.com>
2020-09-19 21:03:36 -0400

    Remove 32-bit ARM support.

--- appveyor.cmd
+++ appveyor.cmd
@@ -8,12 +8,10 @@ msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140 || exit /b
 
 msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141 || exit /b
 msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141 || exit /b
-msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v141 || exit /b
 msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141 || exit /b
 
 msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v142 || exit /b
 msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v142 || exit /b
-msbuild -p:Configuration=Release;Platform=ARM;PlatformToolset=v142 || exit /b
 msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v142 || exit /b
 
 msbuild -p:Configuration=Release -t:PackNativeNuget src\wcautil\wcautil.vcxproj || exit /b
