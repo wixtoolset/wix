dcf13e60f0b165a8942e7e7d98f5f0702f7d1e84
Bob Arnson <bob@firegiant.com>
2020-09-22 21:58:43 -0400

    Remove ARM32

--- appveyor.cmd
+++ appveyor.cmd
@@ -9,12 +9,10 @@ msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v140 || exit /b
 
 msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v141 || exit /b
 msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v141 || exit /b
-msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v141 || exit /b
 msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v141 || exit /b
 
 msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v142 || exit /b
 msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v142 || exit /b
-msbuild -p:Configuration=%_C%;Platform=ARM;PlatformToolset=v142 || exit /b
 msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v142 || exit /b
 
 dotnet test -c %_C% --no-build src\test\WixToolsetTest.Mba.Core\WixToolsetTest.Mba.Core.csproj || exit /b
