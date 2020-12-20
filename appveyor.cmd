61c33d4eafbc9c4a8f12003d46b5027e27beb175
Sean Hall <r.sean.hall@gmail.com>
2020-12-19 22:26:10 -0600

    Don't allow warnings.

--- appveyor.cmd
+++ appveyor.cmd
@@ -2,8 +2,8 @@
 @pushd %~dp0
 @set _C=Release
 
-msbuild -p:Configuration=%_C% -Restore || exit /b
-msbuild -p:Configuration=%_C% src\TestData -Restore || exit /b
+msbuild -p:Configuration=%_C% -warnaserror -Restore || exit /b
+msbuild -p:Configuration=%_C% src\TestData -warnaserror -Restore || exit /b
 
 dotnet test -c %_C% --no-build src\WixToolsetTest.BurnE2E || exit /b
 
