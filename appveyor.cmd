976534ee824bf2a5cbc86764d1edf595ac30087b
Sean Hall <r.sean.hall@gmail.com>
2020-12-20 14:28:49 -0600

    Force managed host projects to be AnyCpu.
    Update WixToolset.Bal.wixext to where it shouldn't rebuild during packing so the tests were run on the shipped code.
    Add 64-bit symbols and use x86 symbols to match the wixlib bind path.

--- appveyor.cmd
+++ appveyor.cmd
@@ -10,7 +10,7 @@ msbuild -p:Configuration=%_C% src\test\examples\examples.proj || exit /b
 dotnet test -c %_C% --no-build src\test\WixToolsetTest.Bal || exit /b
 dotnet test -c %_C% --no-build src\test\WixToolsetTest.ManagedHost || exit /b
 
-msbuild -p:Configuration=%_C% -t:Pack src\wixext\WixToolset.Bal.wixext.csproj || exit /b
+msbuild -p:Configuration=%_C% -p:SkipReferencesToPack=true -t:Pack src\wixext\WixToolset.Bal.wixext.csproj || exit /b
 msbuild -p:Configuration=%_C% -t:Pack src\WixToolset.Mba.Host\WixToolset.Mba.Host.csproj || exit /b
 
 @popd
