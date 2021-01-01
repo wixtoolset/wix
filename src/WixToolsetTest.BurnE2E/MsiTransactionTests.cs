d45c8da71cd9eecdbb314d04754f4316cadeba6f
Sean Hall <r.sean.hall@gmail.com>
2020-12-31 20:30:27 -0600

    Verify MSI transactions are registered and cached appropriately.

--- src/WixToolsetTest.BurnE2E/MsiTransactionTests.cs
+++ src/WixToolsetTest.BurnE2E/MsiTransactionTests.cs
@@ -41,12 +41,16 @@ namespace WixToolsetTest.BurnE2E
 
             bundleAv1.Install();
 
+            var bundleAv1CachedPath = bundleAv1.VerifyRegisteredAndInPackageCache();
+
             // Source file should be installed
             Assert.True(File.Exists(packageASourceCodeInstalled), String.Concat("Should have found Package A payload installed at: ", packageASourceCodeInstalled));
             Assert.True(File.Exists(packageBv1SourceCodeInstalled), String.Concat("Should have found Package Bv1 payload installed at: ", packageBv1SourceCodeInstalled));
             Assert.True(File.Exists(packageCv1SourceCodeInstalled), String.Concat("Should have found Package Cv1 payload installed at: ", packageCv1SourceCodeInstalled));
 
             bundleAv2.Install();
+
+            var bundleAv2CachedPath = bundleAv2.VerifyRegisteredAndInPackageCache();
 
             // Source file should be upgraded
             Assert.True(File.Exists(packageDSourceCodeInstalled), String.Concat("Should have found Package D payload installed at: ", packageDSourceCodeInstalled));
@@ -56,6 +60,8 @@ namespace WixToolsetTest.BurnE2E
             Assert.False(File.Exists(packageBv1SourceCodeInstalled), String.Concat("Package Bv1 payload should have been removed by upgrade uninstall from: ", packageBv1SourceCodeInstalled));
             Assert.False(File.Exists(packageASourceCodeInstalled), String.Concat("Package A payload should have been removed by upgrade uninstall from: ", packageASourceCodeInstalled));
 
+            bundleAv1.VerifyUnregisteredAndRemovedFromPackageCache(bundleAv1CachedPath);
+
             // Uninstall everything.
             bundleAv2.Uninstall();
 
@@ -63,6 +69,8 @@ namespace WixToolsetTest.BurnE2E
             Assert.False(File.Exists(packageDSourceCodeInstalled), String.Concat("Package D payload should have been removed by uninstall from: ", packageDSourceCodeInstalled));
             Assert.False(File.Exists(packageBv2SourceCodeInstalled), String.Concat("Package Bv2 payload should have been removed by uninstall from: ", packageBv2SourceCodeInstalled));
             Assert.False(File.Exists(packageCv2SourceCodeInstalled), String.Concat("Package Cv2 payload should have been removed by uninstall from: ", packageCv2SourceCodeInstalled));
+
+            bundleAv2.VerifyUnregisteredAndRemovedFromPackageCache(bundleAv2CachedPath);
         }
 
         /// <summary>
@@ -95,10 +103,18 @@ namespace WixToolsetTest.BurnE2E
 
             bundleBv1.Install();
 
+            bundleBv1.VerifyRegisteredAndInPackageCache();
+
             // Source file should be installed
             Assert.True(File.Exists(packageBv1SourceCodeInstalled), String.Concat("Should have found Package Bv1 payload installed at: ", packageBv1SourceCodeInstalled));
 
             bundleBv2.Install((int)MSIExec.MSIExecReturnCode.ERROR_INSTALL_FAILURE);
+
+            // Bundle v2 should be registered since it installed a non-permanent package.
+            bundleBv2.VerifyRegisteredAndInPackageCache();
+
+            // Bundle v1 should not have been removed since the install of v2 failed in the middle of the chain.
+            bundleBv1.VerifyRegisteredAndInPackageCache();
 
             // Source file should be installed
             Assert.True(File.Exists(packageASourceCodeInstalled), String.Concat("Should have found Package A payload installed at: ", packageASourceCodeInstalled));
