bd595d0beca939387b3639eb938a4810e53a8338
Sean Hall <r.sean.hall@gmail.com>
2021-01-02 16:00:28 -0600

    Use proper way to pack without building.

--- appveyor.cmd
+++ appveyor.cmd
@@ -10,8 +10,8 @@ msbuild -p:Configuration=%_C% src\test\examples\examples.proj || exit /b
 dotnet test -c %_C% --no-build src\test\WixToolsetTest.Bal || exit /b
 dotnet test -c %_C% --no-build src\test\WixToolsetTest.ManagedHost || exit /b
 
-msbuild -p:Configuration=%_C% -p:SkipReferencesToPack=true -t:Pack src\wixext\WixToolset.Bal.wixext.csproj || exit /b
-msbuild -p:Configuration=%_C% -t:Pack src\WixToolset.Mba.Host\WixToolset.Mba.Host.csproj || exit /b
+msbuild -p:Configuration=%_C% -p:NoBuild=true -t:Pack src\wixext\WixToolset.Bal.wixext.csproj || exit /b
+msbuild -p:Configuration=%_C% -p:NoBuild=true -t:Pack src\WixToolset.Mba.Host\WixToolset.Mba.Host.csproj || exit /b
 
 @popd
 @endlocal
