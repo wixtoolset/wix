a46b1cf9bd6f7dfca72b238a1bb1c838dde558ae
Sean Hall <r.sean.hall@gmail.com>
2021-01-03 14:55:59 -0600

    Update dependencies.

--- appveyor.cmd
+++ appveyor.cmd
@@ -3,12 +3,11 @@
 
 nuget restore || exit /b
 
-msbuild -p:Configuration=Release -t:Restore || exit /b
+msbuild -p:Configuration=Release -Restore || exit /b
 
-msbuild -p:Configuration=Release src\test\WixToolsetTest.Util\WixToolsetTest.Util.csproj || exit /b
 dotnet test -c Release --no-build src\test\WixToolsetTest.Util || exit /b
 
-msbuild -p:Configuration=Release -t:Pack src\wixext\WixToolset.Util.wixext.csproj || exit /b
+msbuild -p:Configuration=Release -p:NoBuild=true -t:Pack src\wixext\WixToolset.Util.wixext.csproj || exit /b
 
 @popd
 @endlocal
