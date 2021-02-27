1550b30702ae4e83ea8b30bf95a4f20a6424aa4b
Rob Mensching <rob@firegiant.com>
2021-02-27 15:38:50 -0800

    Include native .pdbs in .wixext.nupkg and refine build process

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,14 +1,19 @@
 @setlocal
 @pushd %~dp0
+@set _C=Release
+@if /i "%1"=="debug" set _C=Debug
 
-nuget restore || exit /b
+:: Restore
+msbuild -p:Configuration=%_C% -t:Restore || exit /b
 
-msbuild -p:Configuration=Release -t:Restore || exit /b
+:: Build
+msbuild -p:Configuration=%_C% src\test\WixToolsetTest.Dependency\WixToolsetTest.Dependency.csproj || exit /b
 
-msbuild -p:Configuration=Release src\test\WixToolsetTest.Dependency\WixToolsetTest.Dependency.csproj || exit /b
-dotnet test -c Release --no-build src\test\WixToolsetTest.Dependency || exit /b
+:: Test
+dotnet test -c %_C% --no-build src\test\WixToolsetTest.Dependency || exit /b
 
-msbuild -p:Configuration=Release -t:Pack src\wixext\WixToolset.Dependency.wixext.csproj || exit /b
+:: Pack
+msbuild -p:Configuration=%_C% -p:NoBuild=true -t:Pack src\wixext\WixToolset.Dependency.wixext.csproj || exit /b
 
 @popd
 @endlocal
