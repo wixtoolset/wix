68022b358a6d4abecf63a831023f17bdfee472a8
Rob Mensching <rob@firegiant.com>
2021-03-03 13:54:13 -0800

    Update to latest build infrastructure

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,13 +1,19 @@
 @setlocal
 @pushd %~dp0
+@set _C=Release
+@if /i "%1"=="debug" set _C=Debug
 
-nuget restore || exit /b
+:: Restore
+msbuild -p:Configuration=%_C% -t:Restore || exit /b
 
-msbuild -p:Configuration=Release -Restore || exit /b
+:: Build
+msbuild -p:Configuration=%_C% src\test\WixToolsetTest.Netfx\WixToolsetTest.Netfx.csproj || exit /b
 
-dotnet test -c Release --no-build src\test\WixToolsetTest.Netfx || exit /b
+:: Test
+dotnet test -c %_C% --no-build src\test\WixToolsetTest.Netfx || exit /b
 
-msbuild -p:Configuration=Release -p:NoBuild=true -t:Pack src\wixext\WixToolset.NetFx.wixext.csproj || exit /b
+:: Pack
+msbuild -p:Configuration=%_C% -p:NoBuild=true -t:Pack src\wixext\WixToolset.Netfx.wixext.csproj || exit /b
 
 @popd
 @endlocal
