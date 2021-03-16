d056d00e1e1111b6ef68c57f1f03ef6843204723
Rob Mensching <rob@firegiant.com>
2021-03-16 14:43:36 -0700

    Move ResetAcls to Core.Native from Core
    Also simplify copying/moving files directory creation.

--- src/WixToolset.Core.Native/FileSystem.cs
+++ src/WixToolset.Core.Native/FileSystem.cs
@@ -3,8 +3,10 @@
 namespace WixToolset.Core.Native
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Runtime.InteropServices;
+    using System.Security.AccessControl;
 
     /// <summary>
     /// File system helpers.
@@ -19,10 +21,7 @@ namespace WixToolset.Core.Native
         /// <param name="allowHardlink">Allow hardlinks.</param>
         public static void CopyFile(string source, string destination, bool allowHardlink)
         {
-            if (File.Exists(destination))
-            {
-                File.Delete(destination);
-            }
+            EnsureDirectoryWithoutFile(destination);
 
             if (!allowHardlink || !CreateHardLink(destination, source, IntPtr.Zero))
             {
@@ -41,18 +40,36 @@ namespace WixToolset.Core.Native
         /// <param name="destination">The destination file.</param>
         public static void MoveFile(string source, string destination)
         {
-            if (File.Exists(destination))
-            {
-                File.Delete(destination);
-            }
+            EnsureDirectoryWithoutFile(destination);
 
-            var directory = Path.GetDirectoryName(destination);
+            File.Move(source, destination);
+        }
+
+        /// <summary>
+        /// Reset the ACLs on a set of files.
+        /// </summary>
+        /// <param name="files">The list of file paths to set ACLs.</param>
+        public static void ResetAcls(IEnumerable<string> files)
+        {
+            var aclReset = new FileSecurity();
+            aclReset.SetAccessRuleProtection(false, false);
+
+            foreach (var file in files)
+            {
+                new FileInfo(file).SetAccessControl(aclReset);
+            }
+        }
+
+        private static void EnsureDirectoryWithoutFile(string path)
+        {
+            File.Delete(path);
+
+            var directory = Path.GetDirectoryName(path);
+
             if (!String.IsNullOrEmpty(directory))
             {
                 Directory.CreateDirectory(directory);
             }
-
-            File.Move(source, destination);
         }
 
         [DllImport("Kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
