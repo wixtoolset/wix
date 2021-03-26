f85e509dcb6da33c22f45ee27eff428398bbacf2
Rob Mensching <rob@firegiant.com>
2021-03-25 21:59:47 -0700

    Add XML doc to IVariableResolution

--- src/WixToolset.Extensibility/Services/IVariableResolution.cs
+++ src/WixToolset.Extensibility/Services/IVariableResolution.cs
@@ -2,15 +2,29 @@
 
 namespace WixToolset.Extensibility.Services
 {
-#pragma warning disable 1591 // TODO: add documentation
+    /// <summary>
+    /// Result when resolving a variable.
+    /// </summary>
     public interface IVariableResolution
     {
+        /// <summary>
+        /// Indicates if the value contains variables that cannot be resolved yet.
+        /// </summary>
         bool DelayedResolve { get; set; }
 
+        /// <summary>
+        /// Indicates whether a bind variables default value was used in the resolution.
+        /// </summary>
         bool IsDefault { get; set; }
 
+        /// <summary>
+        /// Indicates whether the resolution updated the value.
+        /// </summary>
         bool UpdatedValue { get; set; }
 
+        /// <summary>
+        /// The resolved value.
+        /// </summary>
         string Value { get; set; }
     }
 }
