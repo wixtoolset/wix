d2f27517dffc02b7c50e386793c7ad89762c6b64
Rob Mensching <rob@firegiant.com>
2021-04-21 11:01:10 -0700

    Integrate new build scripts with signing support

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,6 +1,7 @@
 @setlocal
 @pushd %~dp0
 @set _C=Release
+@if /i "%1"=="debug" set _C=Debug
 
 nuget restore || exit /b
 
@@ -17,8 +18,8 @@ msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v142 || exit /b
 
 dotnet test -c %_C% --no-build src\test\WixToolsetTest.Mba.Core\WixToolsetTest.Mba.Core.csproj || exit /b
 
-msbuild -t:Pack -p:Configuration=%_C% src\balutil\balutil.vcxproj || exit /b
-msbuild -t:Pack -p:Configuration=%_C% src\bextutil\bextutil.vcxproj || exit /b
+msbuild -t:PackNative -p:Configuration=%_C% src\balutil\balutil.vcxproj || exit /b
+msbuild -t:PackNative -p:Configuration=%_C% src\bextutil\bextutil.vcxproj || exit /b
 msbuild -t:Pack -p:Configuration=%_C% -p:NoBuild=true src\WixToolset.Mba.Core\WixToolset.Mba.Core.csproj || exit /b
 
 @popd
