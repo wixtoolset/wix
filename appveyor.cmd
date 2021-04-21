8eb98efd2175d9ece2e4639d43081667af9a4990
Rob Mensching <rob@firegiant.com>
2021-04-21 11:01:33 -0700

    Migrate Burn API headers from burn repository

--- appveyor.cmd
+++ appveyor.cmd
@@ -20,6 +20,7 @@ dotnet test -c %_C% --no-build src\test\WixToolsetTest.Mba.Core\WixToolsetTest.M
 
 msbuild -t:PackNative -p:Configuration=%_C% src\balutil\balutil.vcxproj || exit /b
 msbuild -t:PackNative -p:Configuration=%_C% src\bextutil\bextutil.vcxproj || exit /b
+msbuild -t:PackNative -p:Configuration=%_C% src\WixToolset.BootstrapperCore.Native\WixToolset.BootstrapperCore.Native.proj || exit /b
 msbuild -t:Pack -p:Configuration=%_C% -p:NoBuild=true src\WixToolset.Mba.Core\WixToolset.Mba.Core.csproj || exit /b
 
 @popd
