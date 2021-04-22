f39e7a3e164d0736e45049e5726d0da2013da3c9
Rob Mensching <rob@firegiant.com>
2021-04-22 06:31:03 -0700

    Update to latest build infrastructure

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,22 +1,24 @@
 @setlocal
 @pushd %~dp0
+@set _C=Release
+@if /i "%1"=="debug" set _C=Debug
 
 nuget restore || exit /b
 
-msbuild -t:Test -p:Configuration=Release src\test\DUtilUnitTest || exit /b
+msbuild -t:Test -p:Configuration=%_C% src\test\DUtilUnitTest || exit /b
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v140 || exit /b
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v140 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v140 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v140 || exit /b
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v141 || exit /b
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v141 || exit /b
-msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v141 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v141 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v141 || exit /b
+msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v141 || exit /b
 
-msbuild -p:Configuration=Release;Platform=x86;PlatformToolset=v142 || exit /b
-msbuild -p:Configuration=Release;Platform=x64;PlatformToolset=v142 || exit /b
-msbuild -p:Configuration=Release;Platform=ARM64;PlatformToolset=v142 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x86;PlatformToolset=v142 || exit /b
+msbuild -p:Configuration=%_C%;Platform=x64;PlatformToolset=v142 || exit /b
+msbuild -p:Configuration=%_C%;Platform=ARM64;PlatformToolset=v142 || exit /b
 
-msbuild -p:Configuration=Release -t:PackNativeNuget src\dutil\dutil.vcxproj || exit /b
+msbuild -p:Configuration=%_C% -t:PackNative src\dutil\dutil.vcxproj || exit /b
 
 @popd
 @endlocal
