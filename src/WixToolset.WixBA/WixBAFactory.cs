42170f18cf21e6ac018b264f88eab104eacfe6ab
Sean Hall <r.sean.hall@gmail.com>
2021-05-05 18:52:01 -0500

    Update WixBA to Mba.Core 4.0.26.

--- src/WixToolset.WixBA/WixBAFactory.cs
+++ src/WixToolset.WixBA/WixBAFactory.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
+// Identifies the class that derives from IBootstrapperApplicationFactory and is the BAFactory class that gets
+// instantiated by the interop layer
+[assembly: WixToolset.Mba.Core.BootstrapperApplicationFactory(typeof(WixToolset.WixBA.WixBAFactory))]
 namespace WixToolset.WixBA
 {
     using WixToolset.Mba.Core;
