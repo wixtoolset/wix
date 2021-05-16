91a3a42439d87a5b626e61244edcb8c8e231cc5f
Rob Mensching <rob@firegiant.com>
2021-05-16 16:00:41 -0700

    Fix batch file command-line parsing

--- src/build_all.cmd
+++ src/build_all.cmd
@@ -3,7 +3,7 @@
 
 @set _C=Debug
 :parse_args
-@if /i "%1"=="release" set _C=Release& shift
+@if /i "%1"=="release" set _C=Release
 @if not "%1"=="" shift & goto parse_args
 
 @echo build %_C%
--- src/test/run_test.cmd
+++ src/test/run_test.cmd
@@ -1,15 +1,1 @@
-@setlocal
-@pushd %~dp0
-
-@set _C=Debug
-:parse_args
-@if /i "%1"=="release" set _C=Release& shift
-@if not "%1"=="" shift & goto parse_args
-
-@echo Run integration tests %_C%
-
-@SET RuntimeTestsEnabled=true
-@call burn\test_burn.cmd
-
-@popd
-@endlocal
+@call %~dp0test.cmd test %*
