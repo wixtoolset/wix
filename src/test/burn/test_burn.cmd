2d0f96b561cc27331601453dba9cc079c6b04ef4
Rob Mensching <rob@firegiant.com>
2021-12-14 12:18:27 -0800

    Capture logs from burn integration tests

--- src/test/burn/test_burn.cmd
+++ src/test/burn/test_burn.cmd
@@ -12,7 +12,15 @@
 msbuild -t:Build -Restore -p:Configuration=%_C% -warnaserror || exit /b
 msbuild -t:Build -Restore -p:Configuration=%_C% TestData\TestData.proj || exit /b
 
-if /i "%RuntimeTestsEnabled%"=="true" dotnet test -c %_C% --no-build WixToolsetTest.BurnE2E
+@if not "%RuntimeTestsEnabled%"=="true" goto :LExit
 
+reg add HKLM\Software\Policies\Microsoft\Windows\Installer /t REG_SZ /v Logging /d voicewarmupx /f
+reg add HKLM\Software\WOW6432Node\Policies\Microsoft\Windows\Installer /t REG_SZ /v Logging /d voicewarmupx /f
+
+dotnet test -c %_C% --no-build WixToolsetTest.BurnE2E
+
+7z a "..\..\..\build\logs\test_burn_%GITHUB_RUN_ID%.zip" "%TEMP%\*.log" "%TEMP%\..\*.log"
+
+:LExit
 @popd
 @endlocal
