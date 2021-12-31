37790ecdc4509d6dd3533933bb9131823f98a0ed
Sean Hall <r.sean.hall@gmail.com>
2021-12-31 13:07:46 -0600

    Remove redundant -v m.

--- src/wix/wix.cmd
+++ src/wix/wix.cmd
@@ -25,12 +25,12 @@ msbuild wix.sln -p:Configuration=%_C% -nologo -m -bl:..\..\build\logs\wix_build.
 
 
 :: Pre-Publish Test
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.Converters || exit /b
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.Converters.Symbolizer || exit /b
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.Core.Burn || exit /b
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.Core.Native || exit /b
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.CoreIntegration || exit /b
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.Heat || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.Converters || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.Converters.Symbolizer || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.Core.Burn || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.Core.Native || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.CoreIntegration || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.Heat || exit /b
 
 
 :: Publish
@@ -48,15 +48,15 @@ robocopy %_P_OBJ%\WixToolset.Sdk\separate\netcoreapp3.1\buildtasks %_P%\WixTools
 robocopy %_P_OBJ%\WixToolset.Sdk\separate\netcoreapp3.1\heat %_P%\WixToolset.Sdk\tools\netcoreapp3.1 %_RCO%
 robocopy %_P_OBJ%\WixToolset.Sdk\separate\netcoreapp3.1\wix %_P%\WixToolset.Sdk\tools\netcoreapp3.1 %_RCO%
 
-msbuild -t:Publish -p:Configuration=%_C% -v:m -nologo -p:PublishDir=%_P%WixToolset.Sdk\ WixToolset.Sdk\WixToolset.Sdk.csproj || exit /b
+msbuild -t:Publish -p:Configuration=%_C% -nologo -p:PublishDir=%_P%WixToolset.Sdk\ WixToolset.Sdk\WixToolset.Sdk.csproj || exit /b
 
 :: TODO - used by MsbuildFixture.ReportsInnerExceptionForUnexpectedExceptions test
 :: msbuild -t:Publish -Restore -p:Configuration=%_C% -p:TargetFramework=net472 -p:RuntimeIdentifier=linux-x86 -p:PublishDir=%_P%WixToolset.Sdk\broken\net472\ wix\wix.csproj || exit /b
 
 
 :: Test
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.BuildTasks || exit /b
-dotnet test -c %_C% --no-build --nologo -v m test\WixToolsetTest.Sdk || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.BuildTasks || exit /b
+dotnet test -c %_C% --no-build --nologo test\WixToolsetTest.Sdk || exit /b
 
 :: Pack
 msbuild pack_t.proj -p:Configuration=%_C% -nologo -m -bl:..\..\build\logs\wix_pack.binlog || exit /b
