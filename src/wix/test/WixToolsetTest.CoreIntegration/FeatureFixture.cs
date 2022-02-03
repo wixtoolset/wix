bd0206c6746060dc8c7b4f40d7b3f3de9ee51385
Rob Mensching <rob@firegiant.com>
2022-02-03 07:36:01 -0800

    Fix feature/component mapping verification

---
+++ src/wix/test/WixToolsetTest.CoreIntegration/FeatureFixture.cs
@@ -0,0 +1,43 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+namespace WixToolsetTest.CoreIntegration
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using WixBuildTools.TestSupport;
+    using WixToolset.Core.TestPackage;
+    using WixToolset.Data;
+    using Xunit;
+
+    public class FeatureFixture
+    {
+        [Fact]
+        public void CanDetectMissingFeatureComponentMapping()
+        {
+            var folder = TestData.Get(@"TestData");
+
+            using (var fs = new DisposableFileSystem())
+            {
+                var baseFolder = fs.GetFolder();
+                var intermediateFolder = Path.Combine(baseFolder, "obj");
+                var msiPath = Path.Combine(baseFolder, @"bin\test.msi");
+
+                var result = WixRunner.Execute(new[]
+                {
+                    "build",
+                    Path.Combine(folder, "Feature", "PackageMissingFeatureComponentMapping.wxs"),
+                    "-bindpath", Path.Combine(folder, "SingleFile", "data"),
+                    "-intermediateFolder", intermediateFolder,
+                    "-o", msiPath
+                });
+
+                var errors = result.Messages.Where(m => m.Level == MessageLevel.Error);
+                Assert.Equal(new[]
+                {
+                    267
+                }, errors.Select(e => e.Id).ToArray());
+            }
+        }
+    }
+}
