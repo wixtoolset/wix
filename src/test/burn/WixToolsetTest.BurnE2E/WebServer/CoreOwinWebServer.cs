28c0a27ddf03dcf07a11c291699428a32f381fbc
Sean Hall <r.sean.hall@gmail.com>
2022-02-18 17:56:31 -0600

    Handle missing content length with range request and empty files. Add test for server without range request support.

--- src/test/burn/WixToolsetTest.BurnE2E/WebServer/CoreOwinWebServer.cs
+++ src/test/burn/WixToolsetTest.BurnE2E/WebServer/CoreOwinWebServer.cs
@@ -5,8 +5,10 @@ namespace WixToolsetTest.BurnE2E
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.StaticFiles;
     using Microsoft.Extensions.FileProviders;
     using Microsoft.Extensions.FileProviders.Physical;
@@ -15,11 +17,14 @@ namespace WixToolsetTest.BurnE2E
 
     public class CoreOwinWebServer : IWebServer, IFileProvider
     {
+        const string StaticFileBasePath = "/e2e";
+
         private Dictionary<string, string> PhysicalPathsByRelativeUrl { get; } = new Dictionary<string, string>();
 
         private IHost WebHost { get; set; }
 
         public bool DisableHeadResponses { get; set; }
+        public bool DisableRangeRequests { get; set; }
 
         public void AddFiles(Dictionary<string, string> physicalPathsByRelativeUrl)
         {
@@ -38,10 +43,11 @@ namespace WixToolsetTest.BurnE2E
                                    webBuilder.UseUrls("http://localhost:9999");
                                    webBuilder.Configure(appBuilder =>
                                    {
+                                       appBuilder.Use(this.CustomStaticFileMiddleware);
                                        appBuilder.UseStaticFiles(new StaticFileOptions
                                        {
                                            FileProvider = this,
-                                           RequestPath = "/e2e",
+                                           RequestPath = StaticFileBasePath,
                                            ServeUnknownFileTypes = true,
                                            OnPrepareResponse = this.OnPrepareStaticFileResponse,
                                        });
@@ -49,6 +55,42 @@ namespace WixToolsetTest.BurnE2E
                                })
                                .Build();
             this.WebHost.Start();
+        }
+
+        private async Task CustomStaticFileMiddleware(HttpContext context, Func<Task> next)
+        {
+            if (!this.DisableRangeRequests || (!HttpMethods.IsGet(context.Request.Method) && !HttpMethods.IsHead(context.Request.Method)))
+            {
+                await next();
+                return;
+            }
+
+            // Only send Content-Length header.
+            // Don't support range requests.
+            // https://github.com/dotnet/aspnetcore/blob/60abfafe32a4692f9dc4a172665524f163b10012/src/Middleware/StaticFiles/src/StaticFileMiddleware.cs
+            if (!context.Request.Path.StartsWithSegments(StaticFileBasePath, out var subpath))
+            {
+                context.Response.StatusCode = 404;
+                return;
+            }
+
+            var fileInfo = this.GetFileInfo(subpath);
+            if (!fileInfo.Exists)
+            {
+                context.Response.StatusCode = 404;
+                return;
+            }
+
+            var responseHeaders = context.Response.GetTypedHeaders();
+            var fileLength = fileInfo.Length;
+            responseHeaders.ContentLength = fileLength;
+
+            this.OnPrepareStaticFileResponse(new StaticFileResponseContext(context, fileInfo));
+
+            if (HttpMethods.IsGet(context.Request.Method))
+            {
+                await context.Response.SendFileAsync(fileInfo, 0, fileLength);
+            }
         }
 
         private void OnPrepareStaticFileResponse(StaticFileResponseContext obj)
