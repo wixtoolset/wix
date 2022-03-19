b3af703012d79cd7001998dd77f1c63902bdba9a
Rob Mensching <rob@firegiant.com>
2022-03-19 11:47:16 -0700

    Convert .wxl files namespaces
    Fixes 6639

---
+++ src/wix/test/WixToolsetTest.Converters/LocalizationFixture.cs
@@ -0,0 +1,96 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+namespace WixToolsetTest.Converters
+{
+    using System;
+    using System.Xml.Linq;
+    using WixBuildTools.TestSupport;
+    using WixToolset.Converters;
+    using WixToolsetTest.Converters.Mocks;
+    using Xunit;
+
+    public class LocalizationFixture : BaseConverterFixture
+    {
+        [Fact]
+        public void EnsureNoXmlDeclaration()
+        {
+            var parse = String.Join(Environment.NewLine,
+                "<?xml version='1.0' ?>",
+                "<WixLocalization Culture='en-us'>",
+                "  <String Id='SomeId'>Value</String>",
+                "</WixLocalization>");
+
+            var expected = new[]
+            {
+                "<WixLocalization Culture=\"en-us\" xmlns=\"http://wixtoolset.org/schemas/v4/wxl\">",
+                "  <String Id=\"SomeId\">Value</String>",
+                "</WixLocalization>"
+            };
+
+            var document = XDocument.Parse(parse, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+
+            var messaging = new MockMessaging();
+            var converter = new WixConverter(messaging, 2, null, null);
+
+            var errors = converter.ConvertDocument(document);
+            Assert.Equal(2, errors);
+
+            var actualLines = UnformattedDocumentLines(document);
+            WixAssert.CompareLineByLine(expected, actualLines);
+        }
+
+        [Fact]
+        public void EnsureNamespace()
+        {
+            var parse = String.Join(Environment.NewLine,
+                "<WixLocalization Culture='en-us'>",
+                "  <String Id='SomeId'>Value</String>",
+                "</WixLocalization>");
+
+            var expected = new[]
+            {
+                "<WixLocalization Culture=\"en-us\" xmlns=\"http://wixtoolset.org/schemas/v4/wxl\">",
+                "  <String Id=\"SomeId\">Value</String>",
+                "</WixLocalization>"
+            };
+
+            var document = XDocument.Parse(parse, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+
+            var messaging = new MockMessaging();
+            var converter = new WixConverter(messaging, 2, null, null);
+
+            var errors = converter.ConvertDocument(document);
+            Assert.Equal(1, errors);
+
+            var actualLines = UnformattedDocumentLines(document);
+            WixAssert.CompareLineByLine(expected, actualLines);
+        }
+
+        [Fact]
+        public void FixNamespace()
+        {
+            var parse = String.Join(Environment.NewLine,
+                "<WixLocalization Culture='en-us' xmlns='http://schemas.microsoft.com/wix/2006/localization'>",
+                "  <String Id='SomeId'>Value</String>",
+                "</WixLocalization>");
+
+            var expected = new[]
+            {
+                "<WixLocalization Culture=\"en-us\" xmlns=\"http://wixtoolset.org/schemas/v4/wxl\">",
+                "  <String Id=\"SomeId\">Value</String>",
+                "</WixLocalization>"
+            };
+
+            var document = XDocument.Parse(parse, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+
+            var messaging = new MockMessaging();
+            var converter = new WixConverter(messaging, 2, null, null);
+
+            var errors = converter.ConvertDocument(document);
+            Assert.Equal(1, errors);
+
+            var actualLines = UnformattedDocumentLines(document);
+            WixAssert.CompareLineByLine(expected, actualLines);
+        }
+    }
+}
