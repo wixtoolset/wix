29b3cd4339096a78dc23b2d0a6c494705f0f131c
Rob Mensching <rob@firegiant.com>
2022-05-24 21:59:28 -0700

    Rename build.cmd to devbuild.cmd to better represent its purpose
    The build.cmd in the root of the repository is intended for use by
    developers looking to build the full WiX Toolset. Official builds
    use the appropriate build files in the src folder.
    
    Renaming build.cmd to devbuild.cmd better represents its purpose.
    As a bonus, renaming to devbuild.cmd also removes TAB completion
    conflict with the .\build folder.

---
+++ devbuild.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+setlocal
+pushd %~dp0
+
+:parse_args
+if /i "%1"=="release" set _C=Release
+if /i "%1"=="inc" set _INCREMENTAL=1
+if /i "%1"=="incremental" set _INCREMENTAL=1
+if /i "%1"=="clean" set _INCREMENTAL= & set _CLEAN=1
+if not "%1"=="" shift & goto parse_args
+
+if not "%_INCREMENTAL"=="1" call src\clean.cmd
+if not "%_CLEAN%"=="" goto end
+
+src\build_all.cmd %_C%
+
+:end
+popd
+endlocal
