8e1cbe8d7b468553d76c20452561e89726de5c47
Rob Mensching <rob@firegiant.com>
2022-08-08 18:40:25 -0700

    Simplify and fix incremental/clean in build cmds
    Refining this pattern before spreading more widely.

--- src/setup/setup.cmd
+++ src/setup/setup.cmd
@@ -6,12 +6,13 @@
 
 :parse_args
 @if /i "%1"=="release" set _C=Release
-@if /i "%1"=="inc" set _INCREMENTAL=1
-@if /i "%1"=="clean" set _INCREMENTAL= & set _CLEAN=1
+@if /i "%1"=="inc" set _INC=1
+@if /i "%1"=="clean" set _CLEAN=1
 @if not "%1"=="" shift & goto parse_args
 
 :: Clean
-@if NOT "%_INCREMENTAL%"=="" call :clean
+
+@if "%_INC%"=="" call :clean
 @if NOT "%_CLEAN%"=="" goto :end
 
 @echo Building setup %_C%
