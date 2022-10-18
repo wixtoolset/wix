44e69b72d820bcebee45b738372117d35d487404
Rob Mensching <rob@firegiant.com>
2022-10-18 11:57:08 -0700

    Update build dependencies and normalize more projects

--- src/internal/SetBuildNumber/global.json.pp
+++ src/internal/SetBuildNumber/global.json.pp
@@ -1,7 +1,7 @@
 {
   "msbuild-sdks": {
-    "Microsoft.Build.Traversal": "3.0.23",
-    "Microsoft.Build.NoTargets": "3.3.0",
+    "Microsoft.Build.Traversal": "3.2.0",
+    "Microsoft.Build.NoTargets": "3.5.6",
     "WixToolset.Sdk": "{packageversion}"
   },
   "sdk": {
