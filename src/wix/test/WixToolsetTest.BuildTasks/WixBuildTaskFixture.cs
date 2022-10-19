419c66362e214a198c01eca6b3641c4b21976745
Rob Mensching <rob@firegiant.com>
2022-10-19 09:56:21 -0700

    Workaround ARM64 test failure until there is an ARM64 NETFX wix.exe
    Related to 6960

--- src/wix/test/WixToolsetTest.BuildTasks/WixBuildTaskFixture.cs
+++ src/wix/test/WixToolsetTest.BuildTasks/WixBuildTaskFixture.cs
@@ -26,6 +26,14 @@ namespace WixToolsetTest.BuildTasks
         {
             var folder = TestData.Get("TestData", "SimpleMsiPackage", "MsiPackage");
 
+            var wixExeFolder = PublishedWixExeFolder;
+
+            // TODO: See https://github.com/wixtoolset/issues/issues/6960 - fallback to x86 until we have a wix.exe that supports all platforms.
+            if (!Directory.Exists(wixExeFolder))
+            {
+                wixExeFolder = Path.Combine(Path.GetDirectoryName(wixExeFolder), "x86");
+            }
+
             using (var fs = new DisposableFileSystem())
             {
                 var baseFolder = fs.GetFolder();
@@ -54,7 +62,7 @@ namespace WixToolsetTest.BuildTasks
                     PdbType = "Full",
                     PdbFile = new TaskItem(pdbPath),
                     DefaultCompressionLevel = "nOnE",
-                    ToolPath = PublishedWixExeFolder
+                    ToolPath = wixExeFolder
                 };
 
                 var result = task.Execute();
