ed45c8f2ba1636cf0aa3144f7e77aa82bc79303a
Rob Mensching <rob@firegiant.com>
2023-01-13 17:36:24 -0800

    Add TargetFrameworks to ProjectReference to ease handling multi-framework projects
    Closes 7149

---
+++ src/test/wix/TestData/CsprojClassLibraryMultiFramework/MultiFrameworkClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CsprojClassLibraryMultiFramework
+{
+    public class MultiFrameworkClass
+    {
+#if NET
+        public string Name { get; } = ".NET v6.0 MultiFrameworkClass";
+#else
+        public string Name { get; } = ".NETFX v4.8 MultiFrameworkClass";
+#endif
+    }
+}
---
+++ src/test/wix/TestData/CsprojConsoleMultiFramework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CsprojClassLibraryMultiFramework;
+
+namespace CsprojConsoleNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mfc = new MultiFrameworkClass();
+
+            Console.WriteLine("Hello, {0}!", mfc.Name);
+        }
+    }
+}
