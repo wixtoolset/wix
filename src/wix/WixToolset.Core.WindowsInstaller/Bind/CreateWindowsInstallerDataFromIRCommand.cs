9607c3aec6d99662d9efbd2f14fa5ae285c0aac6
Rob Mensching <rob@firegiant.com>
2023-08-07 12:07:39 -0700

    Ensure the Font actions are scheduled for TrueType fonts
    TrueType fonts are denoted by the empty string in the FontTitle field of the
    FileSymbol. That means a non-null FontTitle field value means a font is being
    installed.
    
    Fixes 7593

--- src/wix/WixToolset.Core.WindowsInstaller/Bind/CreateWindowsInstallerDataFromIRCommand.cs
+++ src/wix/WixToolset.Core.WindowsInstaller/Bind/CreateWindowsInstallerDataFromIRCommand.cs
@@ -678,6 +678,8 @@ namespace WixToolset.Core.WindowsInstaller.Bind
             attributes |= (symbol.Attributes & FileSymbolAttributes.Vital) == FileSymbolAttributes.Vital ? WindowsInstallerConstants.MsidbFileAttributesVital : 0;
             row.Attributes = attributes;
 
+            // Note that TrueType fonts are denoted by the empty string in the FontTitle
+            // field. So, non-null means a font is present.
             if (symbol.FontTitle != null)
             {
                 var fontRow = this.CreateRow(symbol, "Font");
