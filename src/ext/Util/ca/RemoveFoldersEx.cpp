e84b6768772c01e44dd55fb583cf78388ec7e48a
Rob Mensching <rob@firegiant.com>
2024-03-22 11:55:43 -0700

    Don't follow junctions when recursing directories.
    When deleting directories recursively, an elevated custom action
    following junctions in a user-writable location could recurse into
    any directory, including some that you might not want to be deleted.
    Therefore, avoid recursing into directories that are actually
    junctions (aka "reparse points").
    
    This applies to:
    
    - The RemoveFoldersEx custom action (which doesn't actually do deletions
    but would instruct elevated MSI to delete on your behalf).
    - DTF's custom action runner.

--- src/ext/Util/ca/RemoveFoldersEx.cpp
+++ src/ext/Util/ca/RemoveFoldersEx.cpp
@@ -38,6 +38,14 @@ static HRESULT RecursePath(
     }
 #endif
 
+    // Do NOT follow junctions.
+    DWORD dwAttributes = ::GetFileAttributesW(wzPath);
+    if (dwAttributes & FILE_ATTRIBUTE_REPARSE_POINT)
+    {
+        WcaLog(LOGMSG_STANDARD, "Path is a junction; skipping: %ls", wzPath);
+        ExitFunction();
+    }
+
     // First recurse down to all the child directories.
     hr = StrAllocFormatted(&sczSearch, L"%s*", wzPath);
     ExitOnFailure(hr, "Failed to allocate file search string in path: %S", wzPath);
@@ -210,10 +218,10 @@ extern "C" UINT WINAPI WixRemoveFoldersEx(
 
         hr = PathExpand(&sczExpandedPath, sczPath, PATH_EXPAND_ENVIRONMENT);
         ExitOnFailure(hr, "Failed to expand path: %S for row: %S", sczPath, sczId);
-        
+
         hr = PathBackslashTerminate(&sczExpandedPath);
         ExitOnFailure(hr, "Failed to backslash-terminate path: %S", sczExpandedPath);
-    
+
         WcaLog(LOGMSG_STANDARD, "Recursing path: %S for row: %S.", sczExpandedPath, sczId);
         hr = RecursePath(sczExpandedPath, sczId, sczComponent, sczProperty, iMode, f64BitComponent, &dwCounter, &hTable, &hColumns);
         ExitOnFailure(hr, "Failed while navigating path: %S for row: %S", sczPath, sczId);
