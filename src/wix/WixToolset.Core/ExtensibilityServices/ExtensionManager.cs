ca8a55ed44bd739e5eb7d77fbcdb3eba3ba0fee8
Rob Mensching <rob@firegiant.com>
2024-04-25 11:12:22 -0700

    Update to wixext folder to v6

--- src/wix/WixToolset.Core/ExtensibilityServices/ExtensionManager.cs
+++ src/wix/WixToolset.Core/ExtensibilityServices/ExtensionManager.cs
@@ -16,7 +16,7 @@ namespace WixToolset.Core.ExtensibilityServices
     internal class ExtensionManager : IExtensionManager
     {
         // This value needs to stay in sync with the Property in "wix.props" with the same name.
-        private const string WixToolsetExtensionPackageFolder = "wixext5";
+        private const string WixToolsetExtensionPackageFolder = "wixext6";
 
         private const string UserWixFolderName = ".wix";
         private const string MachineWixFolderName = "WixToolset";
