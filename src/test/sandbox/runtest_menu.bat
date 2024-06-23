18bb064b71cef2b02eba333cb4f7248ecac628f0
Bevan Weiss <bevan.weiss@gmail.com>
2024-06-23 22:30:38 +1000

    Wrap variable sets in double quotes so special characters don't break them. By default enable /noauth and /anyuser on the debugger, and don't show the security warning (/nosecuritywarn). It'll still show the firewall warning prompt.  Default settings should be fine for firewall (Private networks).
    Signed-off-by: Bevan Weiss <bevan.weiss@gmail.com>

--- src/test/sandbox/runtest_menu.bat
+++ src/test/sandbox/runtest_menu.bat
@@ -9,11 +9,11 @@ REM to change for each invocation of the Sandbox environment
 for /f "tokens=2 delims=[]" %%a in ('ping -n 1 -4 ""') do set IPAddr=%%a
 @if %PROCESSOR_ARCHITECTURE%=="ARM64" (
 	@if exist C:\sandbox\Debugger\ARM64\msvsmon.exe (
-		set MsVsMonPath=C:\sandbox\Debugger\ARM64\msvsmon.exe
+		set "MsVsMonPath=C:\sandbox\Debugger\ARM64\msvsmon.exe /noauth /anyuser /nosecuritywarn"
 	)
 ) else (
 	@if exist C:\sandbox\Debugger\x64\msvsmon.exe (
-		set MsVsMonPath=C:\sandbox\Debugger\x64\msvsmon.exe
+		set "MsVsMonPath=C:\sandbox\Debugger\x64\msvsmon.exe /noauth /anyuser /nosecuritywarn"
 	)
 )
 
@@ -53,13 +53,13 @@ set "TestIs=!option[%SelectTest%]!"
 if not "%SelectTest%"=="0" (
 	REM for the non-Debugger options, we want to get the basepath
 	REM the file name, and a TestName component..
-	for %%a in (%TestIs%) do set FileDir=%%~dpa
-	for %%b in (%TestIs%) do set FileName=%%~nxb
+	for %%a in (%TestIs%) do set "FileDir=%%~dpa"
+	for %%b in (%TestIs%) do set "FileName=%%~nxb"
 
 	REM since we start from C:\build, the 3rd token in '\' is
 	REM the 'IntegrationMsi' naming that looks good for a TestName
 	for /f "tokens=3 delims=\" %%c in ("%TestIs%") do (
-		set TestName=%%c
+		set "TestName=%%c"
 	)
 
 	REM We just start these as a separate window
