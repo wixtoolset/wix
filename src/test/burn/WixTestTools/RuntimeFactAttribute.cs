54715f1cecf12276445ddc8bdb41ac631927456a
Bevan Weiss <bevan.weiss@gmail.com>
2024-07-13 15:52:51 +1000

    Add conditional MsiE2E Tests for Domain functionality. This test currently fails under a Domain workstation.
    Signed-off-by: Bevan Weiss <bevan.weiss@gmail.com>

--- src/test/burn/WixTestTools/RuntimeFactAttribute.cs
+++ src/test/burn/WixTestTools/RuntimeFactAttribute.cs
@@ -3,15 +3,20 @@
 namespace WixTestTools
 {
     using System;
+    using System.DirectoryServices.ActiveDirectory;
     using System.Security.Principal;
     using WixInternal.TestSupport.XunitExtensions;
 
     public class RuntimeFactAttribute : SkippableFactAttribute
     {
         const string RequiredEnvironmentVariableName = "RuntimeTestsEnabled";
+        const string RequiredDomainEnvironmentVariableName = "RuntimeDomainTestsEnabled";
 
         public static bool RuntimeTestsEnabled { get; }
         public static bool RunningAsAdministrator { get; }
+
+        public static bool RuntimeDomainTestsEnabled { get; }
+        public static bool RunningInDomain { get; }
 
         static RuntimeFactAttribute()
         {
@@ -21,6 +26,33 @@ namespace WixTestTools
 
             var testsEnabledString = Environment.GetEnvironmentVariable(RequiredEnvironmentVariableName);
             RuntimeTestsEnabled = Boolean.TryParse(testsEnabledString, out var testsEnabled) && testsEnabled;
+
+            RunningInDomain = false;
+            try
+            {
+                RunningInDomain = !String.IsNullOrEmpty(System.DirectoryServices.ActiveDirectory.Domain.GetComputerDomain().Name);
+            }
+            catch (ActiveDirectoryObjectNotFoundException) { }
+
+            var domainTestsEnabledString = Environment.GetEnvironmentVariable(RequiredDomainEnvironmentVariableName);
+            RuntimeDomainTestsEnabled = Boolean.TryParse(domainTestsEnabledString, out var domainTestsEnabled) && domainTestsEnabled;
+        }
+
+        private bool _domainRequired;
+        public bool DomainRequired
+        {
+            get
+            {
+                return _domainRequired;
+            }
+            set
+            {
+                _domainRequired = value;
+                if (_domainRequired && String.IsNullOrEmpty(this.Skip) && (!RunningInDomain || !RuntimeDomainTestsEnabled))
+                {
+                    this.Skip = $"These tests require the test host to be running as a domain member ({(RunningInDomain ? "passed" : "failed")}). These tests affect both MACHINE AND DOMAIN state. To accept the consequences, set the {RequiredDomainEnvironmentVariableName} environment variable to true ({(RuntimeDomainTestsEnabled ? "passed" : "failed")}).";
+                }
+            }
         }
 
         public RuntimeFactAttribute()
