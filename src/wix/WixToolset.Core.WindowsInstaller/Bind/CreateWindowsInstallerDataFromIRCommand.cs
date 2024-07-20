a9b161c178434dfbb6aa05caff4b9ba10366346c
Bevan Weiss <bevan.weiss@gmail.com>
2024-07-20 13:39:08 +1000

    Introduce early exception for overlength Windows Installer table name
    Signed-off-by: Bevan Weiss <bevan.weiss@gmail.com>

--- src/wix/WixToolset.Core.WindowsInstaller/Bind/CreateWindowsInstallerDataFromIRCommand.cs
+++ src/wix/WixToolset.Core.WindowsInstaller/Bind/CreateWindowsInstallerDataFromIRCommand.cs
@@ -1447,6 +1447,13 @@ namespace WixToolset.Core.WindowsInstaller.Bind
                                 this.Messaging.Write(WarningMessages.DangerousTableInMergeModule(row.SourceLineNumbers, table.Name));
                             }
                         }
+                        else if (31 < table.Name.Length)
+                        {
+                            foreach (var row in table.Rows)
+                            {
+                                this.Messaging.Write(ErrorMessages.OverlengthTableNameInProductOrMergeModule(row.SourceLineNumbers, table.Name));
+                            }
+                        }
                         break;
 
                     case OutputType.PatchCreation:
@@ -1504,6 +1511,13 @@ namespace WixToolset.Core.WindowsInstaller.Bind
                             foreach (var row in table.Rows)
                             {
                                 this.Messaging.Write(WarningMessages.UnexpectedTableInProduct(row.SourceLineNumbers, table.Name));
+                            }
+                        }
+                        else if (31 < table.Name.Length)
+                        {
+                            foreach (var row in table.Rows)
+                            {
+                                this.Messaging.Write(ErrorMessages.OverlengthTableNameInProductOrMergeModule(row.SourceLineNumbers, table.Name));
                             }
                         }
                         break;
