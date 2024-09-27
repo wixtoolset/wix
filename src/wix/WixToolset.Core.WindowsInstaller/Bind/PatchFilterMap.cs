ffd6c812dce538b49655050bc5ad4c5de95c29f7
Bob Arnson <bob@firegiant.com>
2024-09-27 18:12:11 -0400

    Fix crash when adding file in patch when filtering

--- src/wix/WixToolset.Core.WindowsInstaller/Bind/PatchFilterMap.cs
+++ src/wix/WixToolset.Core.WindowsInstaller/Bind/PatchFilterMap.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
 namespace WixToolset.Core.WindowsInstaller.Bind
 {
@@ -53,6 +53,7 @@ namespace WixToolset.Core.WindowsInstaller.Bind
 
             targetFilterId = patchFilter?.TargetFilterId;
             updatedFilterId = patchFilter?.UpdatedFilterId;
+
             return patchFilter != null;
         }
 
