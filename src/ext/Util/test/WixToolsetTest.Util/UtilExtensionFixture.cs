65e68bcfaf4b0ef9796cad4d5a470b3b3e72032f
Bob Arnson <bob@firegiant.com>
2025-07-19 22:25:25 -0400

    Embiggen build perf.
    - Move ext tests to targets for better MSBuild scheduling.
    - Parallelize long-running tests.
    - Disable Firewall ICEs for dev builds; they're redundant with
    round-tripping and ICEs are mutex'd to make them painfully serial.

--- src/ext/Util/test/WixToolsetTest.Util/UtilExtensionFixture.cs
+++ src/ext/Util/test/WixToolsetTest.Util/UtilExtensionFixture.cs
@@ -1,15 +1,18 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+
 namespace WixToolsetTest.Util
 {
-    using System;
     using System.IO;
     using System.Linq;
     using System.Xml;
     using System.Xml.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using WixInternal.MSTestSupport;
     using WixInternal.Core.MSTestPackage;
+    using WixInternal.MSTestSupport;
     using WixToolset.Util;
 
     [TestClass]
--- src/wix/test/WixToolsetTest.Sdk/MsbuildFixture.cs
+++ src/wix/test/WixToolsetTest.Sdk/MsbuildFixture.cs
@@ -1,5 +1,9 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+
 namespace WixToolsetTest.Sdk
 {
     using System;
