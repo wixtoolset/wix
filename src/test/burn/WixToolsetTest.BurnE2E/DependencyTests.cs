b69c073d6f72e290f2fe4a6b194af18898c8f390
JasonLow8 <107228593+JasonLow8@users.noreply.github.com>
2025-07-29 14:23:29 +1200

    Update DependencyTests.cs
    Add test

--- src/test/burn/WixToolsetTest.BurnE2E/DependencyTests.cs
+++ src/test/burn/WixToolsetTest.BurnE2E/DependencyTests.cs
@@ -291,6 +291,57 @@ namespace WixToolsetTest.BurnE2E
             }
         }
 
+        
+        [RuntimeFact]
+        public void CanUninstallPatchBundleFromARP()
+        {
+            var originalVersion = "1.0.0.0";
+            var patchedVersion = "1.0.1.0";
+            var testRegistryValue = "PackageA";
+
+            var packageA = this.CreatePackageInstaller("PackageAv1");
+            var packageEv1 = this.CreatePackageInstaller("PackageEv1");
+            var packageEv101 = this.CreatePackageInstaller("PackageEv1_0_1");
+            var bundleJ = this.CreateBundleInstaller("BundleJ");
+            var bundleJ_Patch = this.CreateBundleInstaller("BundleJ_Patch");
+
+            packageA.VerifyInstalled(false);
+            packageEv1.VerifyInstalledWithVersion(false);
+            packageEv101.VerifyInstalledWithVersion(false);
+
+            bundleJ.Install();
+            if (this.SupportAddonAndPatchRelatedBundles)
+            {
+                bundleJ.VerifyRegisteredAndInPackageCache();
+
+                packageA.VerifyInstalled(true);
+                packageA.VerifyTestRegistryValue(testRegistryValue, originalVersion);
+                packageEv1.VerifyInstalledWithVersion(true);
+            }
+
+            bundleJ_Patch.Install();
+            if (this.SupportAddonAndPatchRelatedBundles)
+            {
+                bundleJ_Patch.VerifyRegisteredAndInPackageCache();
+
+                packageA.VerifyInstalled(true);
+                packageA.VerifyTestRegistryValue(testRegistryValue, patchedVersion);
+                packageEv1.VerifyInstalledWithVersion(false);
+                packageEv101.VerifyInstalledWithVersion(true);
+            }
+
+            bundleJ.Uninstall(0, "-burn.related.patch");
+            if (this.SupportAddonAndPatchRelatedBundles)
+            {
+                bundleJ.VerifyUnregisteredAndRemovedFromPackageCache();
+                bundleJ_Patch.VerifyUnregisteredAndRemovedFromPackageCache();
+
+                packageA.VerifyInstalled(false);
+                packageEv1.VerifyInstalledWithVersion(false);
+                packageEv101.VerifyInstalledWithVersion(false);
+            }
+        }
+
         [RuntimeFact]
         public void CanUninstallBaseWithAddOnsWhenAllSharePackages()
         {
